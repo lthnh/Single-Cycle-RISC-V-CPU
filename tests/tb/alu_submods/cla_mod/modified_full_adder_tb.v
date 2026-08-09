efc472d17e7cf11b523e1c85bc63a2b6e037c441
lthnh <imr3ng4l@gmail.com>
2026-08-09 15:33:57 +0700

    Revert "feat(alu): add 4-bit lookahead submodule testbench"
    This reverts commit 9d7fb4cc8d2cdfdeaf867a26a2c3a7d24a9b8f05.

--- tests/tb/alu_submods/cla_mod/modified_full_adder_tb.v
+++ tests/tb/alu_submods/cla_mod/modified_full_adder_tb.v
@@ -26,7 +26,7 @@ module modified_full_adder_tb;
 
   integer i;
   initial begin
-    for (i = 0; i < 8; i = i+1) begin
+    for (i = 0; i < 8; i++) begin
       a_in = i[0]; b_in = i[1]; c_in = i[2];
       #1;
       `assert_eq(s_out, s_ref)
