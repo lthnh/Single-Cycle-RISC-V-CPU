612ccc4dcea4d8e1145a7cfe1d9d3a636a625008
lthnh <imr3ng4l@gmail.com>
2026-08-09 15:34:04 +0700

    Revert "feat(alu): add 4-bit lookahead submodule testbench"
    This reverts commit 6ca8dd4d36bdd275b4ecadae84e9b9c842aabdb5.

--- tests/tb/alu_submods/cla_mod/four_bit_lookahead_tb.v
+++ tests/tb/alu_submods/cla_mod/four_bit_lookahead_tb.v
@@ -1,43 +1,13 @@
-`timescale 1ns/1ps
-
-`include "alu_submods/cla_mod/cla.vh"
-`include "assert.vh"
-
-module four_bit_lookahead_tb;
-  reg [`CLA_SUBCOMPONENT_BIT_WIDTH-1:0] g_in, p_in;
-  reg c_in;
+module four_bit_lookahead;
+  reg g_in, p_in;
+  wire c_in;
   wire [4:1] c_out;
 
-  four_bit_lookahead fbl_dut (
+  four_bit_lookahead four_bit_lookahead (
     .g_in (g_in),
     .p_in (p_in),
     .c_in (c_in),
     .c_out(c_out)
   );
 
-  wire [`CLA_SUBCOMPONENT_BIT_WIDTH:0] c_ref;
-
-  assign c_ref[0] = c_in;
-
-  genvar i;
-  for (i = 0; i < `CLA_SUBCOMPONENT_BIT_WIDTH; i = i+1) begin : c_ref_gen
-    assign c_ref[i+1] = g_in[i] | (p_in[i] & c_ref[i]);
-  end
-
-  integer j, k, f;
-  initial begin : fbl_tb
-    for (f = 0; f < 16; f = f+1) begin
-      for (j = 0; j < 16; j = j+1) begin
-        for (k = 0; k < 2; k = k+1) begin
-          g_in = f[3:0]; p_in = j[3:0]; c_in = k[0];
-          #1;
-          `assert_eq(c_out[1], c_ref[1])
-          `assert_eq(c_out[2], c_ref[2])
-          `assert_eq(c_out[3], c_ref[3])
-          `assert_eq(c_out[4], c_ref[4])
-        end
-      end
-    end
-    $display("[INFO] %m is passed!");
-  end
 endmodule
